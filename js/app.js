b5a4e6e81a3d7b163c383f2a3475c8f9551355df
samBrindle <brindlejsam@gmail.com>
2022-04-19 22:12:57 -0500

    finally found error preventing clicks from being tracked

--- js/app.js
+++ js/app.js
@@ -15,6 +15,9 @@ let resultButton = document.getElementById('show-results-btn');
 let product1 = null;
 let product2 = null;
 let product3 = null;
+let temp1 = null;
+let temp2 = null;
+let temp3 = null;
 
 let ctx = document.getElementById('myChart');
 
@@ -56,13 +59,13 @@ function getRandomNumber() {
 }
 
 function allUniqueProducts() {
-    while( product1 === product2 || product1 === product3 || product3 === product2 ) {
-        if(product1 == product2) {
+    while( product1 === product2 || product1 === product3 || product3 === product2 || product1 === temp1 || product1 === temp2 || product1 === temp3 || product3 === temp1 || product3 === temp2 || product3 === temp3 || product2 === temp1 || product2 === temp2 || product2 === temp3) {
+        if(product1 == product2 || product1 === temp1 || product1 === temp2 || product1 === temp3) {
+            product1 = getRandomNumber();
+        } else if( product1 == product3 || product3 === temp1 || product3 === temp2 || product3 === temp3) {
+            product3 = getRandomNumber();
+        } else if(product2 == product3 || product2 === temp1 || product2 === temp2 || product2 === temp3){
             product2 = getRandomNumber();
-        } else if( product1 == product3) {
-            product3 = getRandomNumber();
-        } else if(product2 == product3){
-            product3 = getRandomNumber();
         }
     }
 }
@@ -72,20 +75,23 @@ function renderImg() {
     product1 = getRandomNumber();
     product2 = getRandomNumber();
     product3 = getRandomNumber();
-
+    
     allUniqueProducts();
-
+    
+    temp1 = product1;
+    temp2 = product2;
+    temp3 = product3;
     // not giving alt any value
     image1.src = allProductsArray[product1].src;
-    image1.alt = allProductsArray[product1].name;
+    image1.alt = allProductsArray[product1].productName;
     allProductsArray[product1].views++;
     
     image2.src = allProductsArray[product2].src;
-    image2.alt = allProductsArray[product2].name;
+    image2.alt = allProductsArray[product2].productName;
     allProductsArray[product2].views++;
     
     image3.src = allProductsArray[product3].src;
-    image3.alt = allProductsArray[product3].name;
+    image3.alt = allProductsArray[product3].productName;
     allProductsArray[product3].views++;
 }
 
@@ -95,7 +101,7 @@ renderImg();
 // event handlers
 
 function handleClick(event) {
-    let imgClicked = event.currentTarget.alt;
+    let imgClicked = event.target.alt;
 
     //alt is undefined
     console.log('THIS WAS CLICKED >>>', imgClicked);
